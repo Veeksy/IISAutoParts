be2f4764e1540cf017b27b108d943737e16e1568
Xamnack <63778404+Xamnack@users.noreply.github.com>
2023-04-16 19:37:06 +0400

    2

---
+++ AuthWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using IISAutoParts.authCodeDecode;
+using IISAutoParts.DBcontext;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IISAutoParts
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+
+        IISAutoPartsEntities _db;
+
+        public AuthWindow()
+        {
+            InitializeComponent();
+            _db = new IISAutoPartsEntities();
+        }
+
+        private void EnterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (GetUsersInfo(usernameField.Text, passwordField.Password))
+            {
+                MainWindow mw = new MainWindow();
+                mw.Show();
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        private bool GetUsersInfo(string username, string password)
+        {
+            password = AuthController.Encrypt(password);
+            var user =  _db.users.Where(x=>x.login == username && x.password == password).FirstOrDefault();
+            if (user is null)
+                return false;
+            else return true;
+        }
+
+        private void registerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _db.users.Add(new users
+            {
+                login = usernameField.Text,
+                password = AuthController.Encrypt(passwordField.Password),
+                role = "Admin",
+            });
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ authEncryptDecrypt/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IISAutoParts.authCodeDecode
+{
+    public class AuthController
+    {
+        private static string _encryptedPassword;
+
+
+        public static string Encrypt(string password)
+        {
+            _encryptedPassword = "";
+            for (int i = 0; i < password.Length; i++)
+            {
+                char p = password[i];
+                var numberAscii = (int)p;
+                numberAscii += 10;
+                _encryptedPassword += Char.ConvertFromUtf32(numberAscii);
+            }
+            return _encryptedPassword;
+        }
+
+
+        public static string Decrypt(string password)
+        {
+            _encryptedPassword = "";
+            for (int i = 0; i < password.Length; i++)
+            {
+                char p = password[i];
+                var numberAscii = (int)p;
+                numberAscii -= 10;
+                _encryptedPassword += Char.ConvertFromUtf32(numberAscii);
+            }
+            return _encryptedPassword;
+        }
+    }
+}
