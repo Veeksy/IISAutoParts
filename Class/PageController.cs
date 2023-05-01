f34eb788d303d060e53a3c929f72da89c5f1eab7
Xamnack <63778404+Xamnack@users.noreply.github.com>
2023-05-01 13:26:43 +0400

    change .NET platform

---
+++ Class/PageController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace IISAutoParts.Class
+{
+    public static class PageController
+    {
+        public static Frame MainFrame { get; set; }
+        public static Page ActivePage { get; set; }
+
+    }
+}
