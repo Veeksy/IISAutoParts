dd385f819f261ffa7da3605115b5f6b440d2c7a1
Xamnack <63778404+Xamnack@users.noreply.github.com>
2023-05-11 01:47:38 +0400

    1243

---
+++ pages/reportsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IISAutoParts.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для reportsPage.xaml
+    /// </summary>
+    public partial class reportsPage : Page
+    {
+        public reportsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
